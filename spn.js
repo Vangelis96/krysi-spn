cdfde56593be27e358aa61080eb4a4580e6c089c
Vangelis <vangelis.alsghir@students.fhnw.ch>
2022-03-27 19:09:29 +0200

    finished :D

--- spn.js
+++ spn.js
@@ -3,47 +3,19 @@ const bitPermutationArr =    [0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 
 const r = 4;
 const n = 4;
 const m = 4;
-const k = "00111010100101001101011000111111";
-const s = k.length
+const k = '00111010100101001101011000111111';
+const s = k.length;
 
 function test() {
     let x = '0001001010001111';
     let k = '00010001001010001000110000000000';
     let y = '1010111010110100';
-    let bitPermutationArr =    [0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15];
-    let sboxArr = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7];
     let result = x;
 
-    let key = getRoundKey(k, 0, x.length);
-    console.log('Schlüssel 0', key);
-    result = xor(result, key);
-    console.log('Nach XOR', result);
-
-    for (var i = 1; i < 4; i++) {
-        result = sbox(result, sboxArr);
-        console.log('Nach SBox', result);
-        result = bitPermutation(result, bitPermutationArr);
-        console.log('Nach Bitpermutation', result);
-        key = getRoundKey(k, i, x.length);
-        console.log('Rundenschlüssel', key);
-        result = xor(result, key);
-        console.log('Nach XOR', result, 'Runde ', i);
-    }
-
-    result = sbox(result, sboxArr);
-    key = getRoundKey(k, 4, x.length);
-    result = xor(result, key);
-
+    result = runSPN(x, k);
     console.log(result, y == result, y);
-}
-
-function testInverseBox() {
-    let bitPermutationArr =    [0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15];
-    let sboxArr = [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7];
-
-    const inverseBox = inverseSBox(sboxArr, bitPermutationArr);
-
-    console.log(inverseBox);
+    result = runSPN(y, k, false);
+    console.log(result, x == result, x);
 }
 
 function xor(a, b) {
@@ -52,17 +24,6 @@ function xor(a, b) {
         if (e == splittedB[i]) return 0;
         return 1;
     }).join('');
-}
-
-function addZeros(str, amount) {
-    let offset = str.split('').size() % amount;
-
-    while (offset > 0) {
-        str = '0' + str;
-        offset--;
-    }
-
-    return str;
 }
 
 function bitPermutation(str, permMap) {
@@ -81,19 +42,12 @@ function splitInSections(str, sectionSize) {
     return str.match(new RegExp('.{1,' + sectionSize + '}', 'g'));
 }
 
-function inverseSBox(sbox, bitPermutationArr) {
-    const sBoxReversed = sbox.reverse();
-    const inverseBox = [];
-
-    inverseBox.push(...sBoxReversed.map((element, index) => {
-        if (index == 0 || index == sBoxReversed.length - 1) {
-            return element;
-        } else {
-            return sBoxReversed[bitPermutationArr[index]];
-        }
-    }));
-
-    return inverseBox;
+function inverseSBox(sbox) {
+    const inversed = [];
+    sbox.forEach((element, index) => {
+        inversed[element] = index;
+    });
+    return inversed;
 }
 
 function runSPN(text, key, encode = true) {
@@ -106,7 +60,7 @@ function runSPN(text, key, encode = true) {
         sBox = sboxArr;
     } else {
         // bei Entschlüsselung muss das Inverse der sBox verwendet werden
-        sBox = inverseSBox(sboxArr)
+        sBox = inverseSBox(sboxArr);
     }
 
     result = xor(result, roundKey);
@@ -118,37 +72,63 @@ function runSPN(text, key, encode = true) {
         result = xor(result, roundKey);
     }
 
-    result = sbox(result, sboxArr);
-    roundKey = getRoundKey(key, r, text.length);
+    result = sbox(result, sBox);
+    roundKey = getRoundKey(key, r, text.length, encode);
     result = xor(result, roundKey);
 
     return result;
 }
 
-function getRoundKey(key, round, keysize, encode = true) {
-    let roundKey = key.substr(4 * round, keysize);
+function getRoundKey(key, round, keysize, encode = true, rounds = 4) {
     if (encode) {
         // bei Verschlüsselung wird der Runden-Schlüssel unverändert zurückgegeben
+        let roundKey = key.substr(4 * round, keysize);
         return roundKey;
     } else {
         // bei Entschlüsselung wird der Runden-Schlüssel mittels Bit-Permutation angepasst
+        let roundKey = key.substr(4 * (rounds - round), keysize);
+        if (round == 0 || round == rounds) {
+            return roundKey;
+        }
         return bitPermutation(roundKey, bitPermutationArr);
     }
 }
 
-function decodeCTR(y, key) {
-    splittedY = splitInSections(y, n*m)
+function decodeCTR(y) {
+    const splittedY = splitInSections(y, 16);
+    console.log(y, splittedY);
 
     // shift() gibt das erste Element zurück und entfernt es aus dem Array
-    prevY = splittedY.shift()   // y-1
+    const prevY = splittedY.shift();   // y-1
+    console.log(prevY);
+    console.log(parseInt(prevY, 2));
+    console.log(parseInt(prevY, 2) + 1);
+    console.log((parseInt(prevY, 2) + 1) % Math.pow(2, 16));
+    console.log(((parseInt(prevY, 2) + 1) % Math.pow(2, 16)).toString(2));
 
     // einzelne Teil-Texte entschlüsseln und zurückgeben
     return splittedY.map((yi, i) => {
         //Parameter berechnen
-        text = (prevY + i) % Math.pow(2, 16);
+        const text = ('0000000000000000' + ((parseInt(prevY, 2) + i) % Math.pow(2, 16)).toString(2)).slice(-16);
         //mit SPN entschlüsseln
-        spn = runSPN(text, k, false);
+        const spn = runSPN(text, k);
+        console.log(spn);
         //XOR mit aktuellem Chiffretext
         return xor(spn, yi);
-    })
+    });
+}
+
+function removeTrailingOneZeros(str) {
+    strArr = str.split('');
+    while (strArr.pop() == 0) {}
+    return strArr.join('');
+}
+
+function runBonusaufgabe() {
+    const x = decodeCTR('00000100110100100000101110111000000000101000111110001110011111110110000001010001010000111010000000010011011001110010101110110000');
+    document.getElementById('result').innerText = 'x = ' + x.join('');
+    const truncatedX = removeTrailingOneZeros(x.join(''));
+    const truncatedSplittedX = splitInSections(truncatedX, 8);
+    console.log(truncatedSplittedX);
+    document.getElementById('resultText').innerText = 'Entschlüsselter Text: ' + truncatedSplittedX.map(o => String.fromCharCode(parseInt(o, 2))).join('');
 }
